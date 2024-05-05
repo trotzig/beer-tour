9709e67f3fe5c7dce7f1e100fc6baf3ca0991d6f
Henric Trotzig <henric.trotzig@gmail.com>
2024-05-05 14:23:06 +0200

    Fetch data from spreadsheet
    I've prepped a spreadsheet that holds all the standings

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,52 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { google } from 'googleapis';
+
+const { GOOGLE_API_KEY } = process.env;
 
 async function getData() {
+  // Initialize the Sheets API client
+  const sheets = google.sheets({
+    version: 'v4',
+    auth: GOOGLE_API_KEY,
+  });
+
+  // Fetch data from Google Sheets
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: '1wpLkUl-P-OMEgXE2w98aQ3wbKp62__-TR5wjFxS3zlU',
+    range: 'standings',
+  });
+
+  const values = response.data.values;
+  const players = values.splice(0, 1)[0].slice(1).map(name => {
+    return { name, points: 0 };
+  });
+  for (const row of values) {
+    let i = 0;
+    for (const debt of row.slice(1)) {
+      players[i].points += parseInt(debt, 10);
+      i++;
+    }
+  }
+  const leaderboard = players.sort((a, b) => b.points - a.points);
+
+  leaderboard.forEach((entry, i) => {
+    const previous = (leaderboard[i-1] || { points: -1 });
+    if (previous.points === entry.points) {
+      if (!previous.position.startsWith('T')) {
+        previous.position = `T${previous.position}`;
+      }
+      entry.position = previous.position;
+    } else {
+      entry.position = `${i+1}`;
+    }
+  });
+
+  console.log({ leaderboard, values });
+
   return {
-    leaderboard: [
-      { name: 'Johan Stål', points: 5 },
-      { name: 'Henric Trotzig', points: 3 },
-      { name: 'Jonas Rehman', points: 3 },
-      { name: 'Henrik Persson', points: 2 },
-      { name: 'David Josephson', points: 0 },
-    ],
+    leaderboard,
+    values,
   };
 }
 
@@ -24,14 +61,16 @@ export default async function Home() {
       <table>
         <thead>
           <tr>
+            <th>Pos</th>
             <th>Spelare</th>
-            <th>Öl</th>
+            <th>Intjänade öl</th>
           </tr>
         </thead>
         <tbody>
           {leaderboard.map(entry => {
             return (
               <tr key={entry.name}>
+                <td>{entry.position}</td>
                 <td>{entry.name}</td>
                 <td>{entry.points}</td>
               </tr>
