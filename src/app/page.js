77bfd255792dde8e71a949efe8502919e20ba0b1
Henric Trotzig <henric.trotzig@gmail.com>
2024-05-05 12:54:46 +0200

    First static version

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,44 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
-export default function Home() {
+async function getData() {
+  return {
+    leaderboard: [
+      { name: 'Johan Stål', points: 5 },
+      { name: 'Henric Trotzig', points: 3 },
+      { name: 'Jonas Rehman', points: 3 },
+      { name: 'Henrik Persson', points: 2 },
+      { name: 'David Josephson', points: 0 },
+    ],
+  };
+}
+
+export default async function Home() {
+  const { leaderboard } = await getData();
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div className={styles.beer}>🍺⛳️</div>
+      <h1>Öltouren 2024</h1>
+      <p>Aktuell ställning i Öltouren på Stockholms Golfklubb.</p>
+      <h2>Ledartavla</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Spelare</th>
+            <th>Öl</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboard.map(entry => {
+            return (
+              <tr key={entry.name}>
+                <td>{entry.name}</td>
+                <td>{entry.points}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </main>
   );
 }
